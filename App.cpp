4ae75ae41d3da5b7db425e03994f3169e48bf930
Kushal Pathak <kushalpathak80@gmail.com>
2023-02-28 20:32:42 +0545

    frustrated! collision detection sucks

--- App.cpp
+++ App.cpp
@@ -3,19 +3,12 @@
 #include <windows.h>
 #include <cmath>
 #define block (char)254
-#define cloud 'c'
 #define h 15
 #define w 51
 #define chicken_height 5
 #define chicken_width 6
 using namespace std;
 
-struct obs {
-	float x;
-	int height;
-};
-const int no_of_obstacle = 2;
-obs obstacles[no_of_obstacle];
 string chicken_body = "...\\\\....(o>\\\\_//).\\_/_).../\\.";
 char buffer[h][w], play='y';
 char chicken[chicken_height][chicken_width];
@@ -41,6 +34,7 @@ void game_over_message();
 void reset_game();
 void update_obstacles();
 
+/////////////////////////////////////////////////////////
 int main() {
 	srand((unsigned int)time(0));
 	while (play == 'y') {
@@ -57,56 +51,24 @@ int main() {
 	}
 	return 0;
 }
-
-void update_obstacles() {
-	for (int i = 0; i < no_of_obstacle; i++) {
-		obstacles[i].x -= 1;
-	}
-}
+////////////////////////////////////////////////////////
 
 void bind_obstacles() {
-	for (int i = 0; i < no_of_obstacle; i++) {
-		int tx = obstacles[i].x;
-		for (int j = 1; j <= obstacles[i].height; j++) {
-			buffer[h - 1 - j][tx] = block;
-		}
-	}
+	
 }
 void unbind_obstacles() {
-	for (int i = 0; i < no_of_obstacle; i++) {
-		int tx = obstacles[i].x;
-		for (int j = 1; j <= obstacles[i].height; j++) {
-			buffer[h - 1 - j][tx] = ' ';
-		}
-	}
+	
 }
 void detect_collision() {
-	int ty, tx;
-	/*for (int i = 0; i < chicken_height; i++) {
-		for (int j = 0; j < chicken_width; j++) {
-			tx = (int)x_off + j; ty = (int)y_off + i;
-			if (buffer[ty][tx+1] == block && chicken[i][j]!=' ') {
-				game_over = 1;
-				Beep(300, 1000);
-				return;
-			}
-		}
-	}*/
+	
 }
 
 void update() {
 	step++;
 	count_score();
-	//detect_collision();
 	unbind_chicken();
-	unbind_obstacles();
 	y_off += vy;
-	update_obstacles();
-	detect_collision();
-	bind_obstacles();
 	bind_chicken();
-	shift_objects();
-	generate_obstacle();
 }
 
 void bind_chicken() {
@@ -128,31 +90,8 @@ void count_score() {
 }
 
 void generate_obstacle() {
-	if (!obstacles) {
-		obstacles[0].height = 1 + rand() % 3;
-		obstacles[0].x = w - 1;
-		for (int i = 1; i < no_of_obstacle; i++) {
-			int height = 1 + rand() % 3;
-			obstacles[i].height = height;
-			obstacles[i].x = obstacles[i - 1].x + 25;
-		}
+	
 	}
-	else {
-		for (int i = 0; i < no_of_obstacle; i++) {
-			if (obstacles[i].x < 0) {
-				int height = 1 + rand() % 3;
-				obstacles[i].height = height;
-				obstacles[i].x = w - 1;
-			}
-		}
-	}
-	/*if (step % 35 == 0) {
-		int size = 1 + rand() % 3;
-		for (int i = 1; i <= size; i++) {
-			buffer[h - 1 - i][w-1] = block;
-		}
-	}*/
-}
 void shift_objects() {
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < w - 2; j++) {
